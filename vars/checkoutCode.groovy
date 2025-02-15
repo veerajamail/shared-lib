42ec532c75a3c59d60bb06071c781d6dab473ee1
veerajamail <veerajamail@gmail.com>
2025-02-15 19:34:15 +0000

    updated

--- vars/checkoutCode.groovy
+++ vars/checkoutCode.groovy
@@ -1,10 +1,11 @@
+/*
 #def call(String repoUrl, String branch){
 #   def workingDir = "${env.WORKSPACE}"
 #   sh "git clone ${repoUrl} ${workingDir}"
 #   sh "git checkout ${branch}"
 #   return workingDir
 #}
-
+*/
 
 def call(Map config = [:]) {
     def repoUrl = config.repoUrl ?: error("Missing 'repoUrl'")
