85817048f07690dfa873aa3292355e1d7e95723c
veerajamail <veerajamail@gmail.com>
2025-02-15 20:36:38 +0000

    updated

--- vars/checkoutCode.groovy
+++ vars/checkoutCode.groovy
@@ -1,26 +1,8 @@
-/*
-#def call(String repoUrl, String branch){
-#   def workingDir = "${env.WORKSPACE}"
-#   sh "git clone ${repoUrl} ${workingDir}"
-#   sh "git checkout ${branch}"
-#   return workingDir
-#}
-*/
-
-def call(Map config = [:]) {
-    def repoUrl = config.repoUrl ?: error("Missing 'repoUrl'")
-    def branch = config.branch ?: 'main'
-    def credentialsId = config.credentialsId ?: 'github-token'
-
+def call(Map stageParams) {
+ 
     checkout([
         $class: 'GitSCM',
-        branches: [[name: "*/${branch}"]],
-        doGenerateSubmoduleConfigurations: false,
-        extensions: [],
-        submoduleCfg: [],
-        userRemoteConfigs: [[
-            url: repoUrl,
-            credentialsId: credentialsId
-        ]]
+        branches: [[name:  stageParams.branch ]],
+        userRemoteConfigs: [[ url: stageParams.url ]]
     ])
-}
+  }
